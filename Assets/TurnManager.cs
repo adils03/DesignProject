c2cfd4670d736908253b0fd0a0ba2128afea349e
bburaakk <149411618+bburaakk@users.noreply.github.com>
2023-11-13 15:25:34 +0300

    TurnManager Taslak

---
+++ Assets/TurnManager.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Oyuncuları temsil eden sınıf
+public class Player: MonoBehaviour
+{
+    public void StartTurn()
+    {
+        Debug.Log("Oyuncunun sırası başladı.");
+    }
+}
+
+
+//Oyuncuların sırasını yönetir.Players adında bir liste ve turnQueue adında bir kuyruk içerir.
+public class TurnManager : MonoBehaviour                 
+{
+    public List<Player> players = new List<Player>();
+
+    private Queue<Player> turnQueue = new Queue<Player>();
+
+
+    //Start metodu, oyun başladığında çağrılır ve tüm oyuncuları sırayla kuyruğa ekler.
+    void Start()
+    {
+        foreach (Player player in players)
+        {
+            turnQueue.Enqueue(player);
+        }
+
+        StartTurn();
+    }
+
+    //StartTurn metodu, bir oyuncunun sırasını başlatır. Eğer tüm oyuncuların sırası biterse, kuyruğu yeniden doldurur.
+    void StartTurn()
+    {
+        if (turnQueue.Count == 0)
+        {
+            foreach (Player player in players)
+            {
+                turnQueue.Enqueue(player);
+            }
+        }
+
+        Player currentPlayer = turnQueue.Dequeue();
+
+        currentPlayer.StartTurn();
+    }
+
+    //EndTurn metodu geçerli oyuncunun sırasını bitirir ve bir sonraki oyuncunun sırasını başlatır.
+    public void EndTurn()
+    {
+        StartTurn();
+    }
+}
