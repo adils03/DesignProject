9e4d25d1216f935f2ca56b7388bff0e716589ee2
ibranew <ibraglbk@gmail.com>
2023-11-18 15:58:04 +0300

    1811231557

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -11,5 +11,44 @@ public class Hex : MonoBehaviour
     {
         Debug.Log(x + ":" + y + "tiklandi");
     }
+    public bool HasAdvantage { get; set; } // Toprağın avantajı var mı?
+    public bool HasFarmBuilding { get; set; } // Çiftlik binası var mı?
+    public bool HasSoldier { get; set; } // Asker var mı?
+    
+    // Dezavantajları kontrol etmek için bir metot
+    public bool HasDisadvantage()
+    {
+        // Burada toprağın üzerindeki nesnelere göre dezavantajları kontrol edebilirsiniz
+        // Örneğin, ağaç varsa dezavantaj vardır.
+        return false; // Eğer dezavantaj yoksa false döndürün
+    }
+
+    // Asker veya bina olup olmadığını kontrol etmek için bir metot
+    public bool HasSoldierOrBuilding()
+    {
+        return HasSoldier || HasFarmBuilding;
+    }
+
+    // Toprağın avantajını kontrol etmek için bir metot
+    public int GetAdvantageValue(int baseIncome)
+    {
+        if (HasAdvantage)
+        {
+            return baseIncome + 5; // Eğer avantaj varsa geliri artır
+        }
+        else
+        {
+            return baseIncome; // Avantaj yoksa normal geliri döndür
+        }
+    }
+
+    // Dezavantaj durumunda gelir düşüşünü belirlemek için bir metot
+    public int GetDisadvantageValue()
+    {
+        // Eğer dezavantaj varsa, ne kadar gelir kaybı olacağını burada belirleyebilirsiniz.
+        return 0; // Örneğin, ağaç olduğunda gelir kaybı sıfırdır.
+    }
+
+
 
 }
