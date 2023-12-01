006dd703f60ba3d27e46d4555b1bd93e35b970ad
ibranew <ibraglbk@gmail.com>
2023-12-02 02:23:46 +0300

    02.12.23
    Enumlu bir örnek koydum

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -1,116 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SoldierLevel
+{
+    Level1,
+    Level2,
+    Level3,
+    Level4
+}
+
 public class EconomyManager : MonoBehaviour
 {
-    public int totalHexagons = 4; // sahip olunan toprak sayısı 
-    
-    public int baseIncomePerHexagon = 3;// el başına toprak geliri
-   
-    public int soldier1Salary = 4; // level 1 asker maaşı
-    public int soldier2Salary = 10; 
-    public int soldier3Salary = 30; 
-    public int soldier4Salary = 60; 
+    public int TotalHexagons { get; set; } = 4;
+    public int BaseIncomePerHexagon { get; set; } = 3;
 
-    private int totalIncome;// toplam gelir
-    private int totalSalaries;// toplam maaş gideri
-    private int currentGold = 0;// kasada bulunan altın miktarı
-    private List<Hex> ownedHexagons;
+    private int totalIncome;
+    private int totalSalaries;
+    private int currentGold = 0;
+    private List<Hex> ownedHexagons = new List<Hex>();
+
+    // Asker maaşlarını depolamak için sözlük
+    private Dictionary<SoldierLevel, int> soldierSalaries = new Dictionary<SoldierLevel, int>();
 
     void Start()
     {
         CalculateIncome();
         CalculateSalaries();
-        ApplyDisadvantages(); // Dezavantajları uygula
+        ApplyDisadvantages();
         UpdateGold();
     }
 
-   void CalculateIncome()//  gelir hesabı
-   {
-    totalIncome = totalHexagons * baseIncomePerHexagon;
-
-    // Toprakların avantajlarına göre geliri güncelle
-    foreach (Hex hexagon in ownedHexagons)
+    void InitializeSoldierSalaries()
     {
-        totalIncome = hexagon.GetAdvantageValue(totalIncome);
-        // toprak eğer avantaja sahip ise üzerine farm binası eklenmişse
-        // bu GetAdvantageValue metot gelire + 5 gold ekliyor tümsahip olunan topraklar için 
-        // bu durum kontrol ediliyor
-    }
+        soldierSalaries[SoldierLevel.Level1] = 4;
+        soldierSalaries[SoldierLevel.Level2] = 10;
+        soldierSalaries[SoldierLevel.Level3] = 30;
+        soldierSalaries[SoldierLevel.Level4] = 60;
     }
 
-    void CalculateSalaries()// maaş gider hesabı
+    void CalculateIncome()
     {
+        totalIncome = TotalHexagons * BaseIncomePerHexagon;
 
-    int level1Soldiers = GetLevel1SoldierCount() * soldier1Salary;
-    int level2Soldiers = GetLevel2SoldierCount() * soldier2Salary;
-    int level3Soldiers = GetLevel3SoldierCount() * soldier3Salary;
-    int level4Soldiers = GetLevel4SoldierCount() * soldier4Salary;
-
-    totalSalaries = level1Soldiers + level2Soldiers + level3Soldiers + level4Soldiers;
-    }
-
-    void ApplyDisadvantages()// DEzavantajları uygula
-    {
-        // Her toprak için dezavantajı kontrol et
-        // burda hexagon sınıfını tanımlayıp HasDisadvantage metodunu düzenlemeliyiz
-        // kısaca hexagon üzerinde dezavantaj durumu var mı(üerinde ağaç varsa gelir getirmez)
         foreach (Hex hexagon in ownedHexagons)
         {
-            if (hexagon.HasDisadvantage()) // Eğer toprakta dezavantaj varsa
+            totalIncome = hexagon.GetAdvantageValue(totalIncome);
+        }
+    }
+
+    void CalculateSalaries()
+    {
+        int totalSalaries = 0;
+
+        foreach (SoldierLevel level in Enum.GetValues(typeof(SoldierLevel)))
+        {
+            int soldierCount = GetSoldierCount(level);
+            int salary = soldierSalaries[level];
+            totalSalaries += soldierCount * salary;
+        }
+
+        this.totalSalaries = totalSalaries;
+    }
+
+    void ApplyDisadvantages()
+    {
+        foreach (Hex hexagon in ownedHexagons)
+        {
+            if (hexagon.HasDisadvantage())
             {
-                totalIncome -= hexagon.GetDisadvantageValue(); // Dezavantaj değerini gelirden çıkar
+                totalIncome -= hexagon.GetDisadvantageValue();
             }
         }
     }
 
-    void updateAllWithTurn()// tur başına oyuncuların toprak sayısı ve ekonomisi ve asker sayısı güncellenir
-    {
-        //totalHexagons = ownedHexagons.Count();
-          
-    }
-
-    void UpdateGold()// el başı kasadaki altını güncelle
+    void UpdateGold()
     {
         currentGold += totalIncome - totalSalaries;
         Debug.Log("Total Gold: " + currentGold);
     }
 
-    int GetLevel1SoldierCount()
+    int GetSoldierCount(SoldierLevel level)
     {
-        // 1. seviye asker sayısını buradan alabilirsiniz.
-        // burda asker sayısı ve maaşları çarparak maliyet hesabı yapılır
+        // Gerçek oyun mantığına göre asker sayısını al
         // Örneğin: return 10;
         return 0;
     }
-    int GetLevel2SoldierCount()
-    {
-        // 2. seviye asker sayısını buradan alabilirsiniz.
-         // burda asker sayısı ve maaşları çarparak maliyet hesabı yapılır
-        // Örneğin: return 10;
-        return 0;
-    }
-    int GetLevel3SoldierCount()
-    {
-        // 3. seviye asker sayısını buradan alabilirsiniz.
-         // burda asker sayısı ve maaşları çarparak maliyet hesabı yapılır
-        // Örneğin: return 10;
-        return 0;
-    }
-    int GetLevel4SoldierCount()
-    {
-        // 4. seviye asker sayısını buradan alabilirsiniz.
-         // burda asker sayısı ve maaşları çarparak maliyet hesabı yapılır
-        // Örneğin: return 10;
-        return 0;
-    }
-    int GetTotalSoldierCount()
-    {
-    int totalcount = GetLevel1SoldierCount() + GetLevel2SoldierCount() + GetLevel3SoldierCount() + GetLevel4SoldierCount();
-    return totalcount;
-    }
-
 }
 
 
