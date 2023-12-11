1bf7d97c1a6adde58f47fb76f2e0d3a096426c60
ibranew <ibraglbk@gmail.com>
2023-12-11 21:23:44 +0300

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -240,8 +240,9 @@ public class SpawnManager : MonoBehaviour
         {
             spreadLimit = 8;
         }
-        else if(hexesWithOutTreeCount < 4)
+        else if(hexesWithOutTreeCount < 14)
         {
+            spreadLimit = 0;
             canSpread = false;
         }
 
@@ -298,66 +299,3 @@ public class SpawnManager : MonoBehaviour
         }
         ProtectedHexes.Clear();
     }
-
-
-
-    /* public class Tower : Unite
- {
-     SpawnManager spawnManager;
-     GridSystem gridSystem;
-     public List<Hex> ProtectedHexes = new List<Hex>();
-      private void Awake() {
-         spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
-         gridSystem = GameObject.Find("GridSystem").GetComponent<GridSystem>();
-     }
-
-
-
-     public void DestroyTower()
-     {
-         foreach (Hex hex in ProtectedHexes)
-         {
-             hex.SetProtected = false;
-         }
-         ProtectedHexes.Clear();
-     }
-
-     private List<Hex> GetNeighboringHexes(int x, int y)
-     { 
-         Hex hex = gridSystem.FindHex(x, y); 
-         return new List<Hex>();
-     }*/
-
-    /*public void BuildTower(int x, int y)
-    {
-        SpawnBuilding(x, y);
-
-        Hex hex = gridSystem.FindHex(x, y);
-        Vector3 position = gameObject.transform.position;
-        Debug.Log("Objenin X koordinatı: " + position.x + ", Y koordinatı: " + position.y);
-
-        
-        List<Hex> neighboringHexes = hex.neighbors;
-        foreach (Hex neighbour in neighboringHexes)
-        {
-            housePrefab.GetComponent<Tower>().ProtectedHexes.Add(neighbour);
-            neighbour.SetProtected = true;
-            Vector3 neighbourPosition = neighbour.transform.position;
-            Debug.Log("Komşu Hex'in X koordinatı: " + neighbourPosition.x + ", Y koordinatı: " + neighbourPosition.y);
-        }
-        
-    }
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.X))
-        {
-            BuildTower(0,0);
-        }
-        if(Input.GetKey(KeyCode.C))
-        {
-            BuildHouse(3,2);
-        }
-    }
-*/
-
-}
